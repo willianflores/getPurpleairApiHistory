460489b5a3011347843b840904a044e5de7fc86b
Willian Flores <willianflores@gmail.com>
2022-12-28 16:36:24 -0500

    Edited authorship information

--- getPurpleairApiHistory.R
+++ getPurpleairApiHistory.R
@@ -1,13 +1,11 @@
 #------------------------------------------------------------------------------------------#
 #   Author: Antonio Willian Flores de Melo                                                 #
 #           Doctor in Tropical Forests Sciences                                            #
-#                                                                                          #
 #   Institution: Federal University of Acre                                                #
 #                Multidisciplinary Center - Campus Floresta                                #
 #                Estrada do Canela Fina, km 12                                             #
 #                Gleba Formoso, 66980-000                                                  #
 #                Cruzeiro do Sul, Acre, Brasil                                             #
-#                                                                                          #
 #   E-mails: willianflores@ufac.br                                                         #
 #            willianflores@gmail.com                                                       #
 #------------------------------------------------------------------------------------------#
